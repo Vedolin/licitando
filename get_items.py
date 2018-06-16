d9c365ac907aa12108ec154ab0ed6169d2cc1af3
henrique <henrique.silva@onebox.net.br>
2018-06-16 20:21:32 -0300

    doing extractions

--- get_items.py
+++ get_items.py
@@ -7,4 +7,20 @@ response = requests.request("GET", url, headers=headers)
 
 extraction = json.loads(response.text)
 
-print(type(extraction))
+situacoes = set()
+
+# {'ANÁLISE DE RECURSOS', 'AGUARDANDO PRAZO ABERTURA SESSÃO PÚBLICA-RETOMADA', 'PREGÃO SUSPENSO', 'EDITAL PUBLICADO', 'PRAZO PARA MEMORIAIS', 'AGUARDANDO RECEBIMENTO DE PROPOSTAS', 'AGUARDANDO ABERTURA SESSÃO PÚBLICA-RETOMADA', 'ADJUDICAÇÃO AUTORIDADE', 'ELABORAÇÃO DA ATA', 'PRAZO PARA CONTRARRAZÕES', 'HOMOLOGAÇÃO'}
+
+situacao = 'EDITAL PUBLICADO'
+
+for item in extraction:
+    url = "https://www.bec.sp.gov.br/BEC_API/API/pregaoM/NegociacaoItemOC/{}".format(item['Codigo'])
+    response = requests.request("GET", url, headers=headers)
+    ocs = json.loads(response.text)
+
+    for oc in ocs:
+        situacoes.add(oc['SITUACAO'])
+        if situacao == oc['SITUACAO']:
+            print(oc)
+
+print(situacoes)
