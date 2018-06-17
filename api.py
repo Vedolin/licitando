7bc1c4408707dd24136f2d74f2d5f55fb0f3a2f7
henrique <henrique.silva@onebox.net.br>
2018-06-16 21:58:12 -0300

    Extraction completed ocs_full_data.json

--- api.py
+++ api.py
@@ -4,11 +4,13 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Api(Resource):
+
+class ApiResource(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(Api, '/')
+
+api.add_resource(ApiResource, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
