907f6565e2ca1ad47f2e280fb0d14affd9d0f7f6
henrique <henrique.silva@onebox.net.br>
2018-06-16 21:18:26 -0300

    Extraction completed

--- get_items.py
+++ get_items.py
@@ -1,26 +1,37 @@
 import requests
 import json
+import codecs
 
+
+def save_json_file(data, file_name, file_ext):
+    with open('{}.{}'.format(file_name, file_ext), 'wb') as f:
+        json.dump(data, codecs.getwriter('utf-8')(f), ensure_ascii=False)
+
+
+# situacoes = set()
 url = "https://www.bec.sp.gov.br/BEC_API/API/pregaoM/NegociacaoItemOC"
 headers = {}
 response = requests.request("GET", url, headers=headers)
 
 extraction = json.loads(response.text)
+save_json_file(extraction, 'items', 'json')
 
-situacoes = set()
+# situacao = 'EDITAL PUBLICADO'
 
-# {'ANÁLISE DE RECURSOS', 'AGUARDANDO PRAZO ABERTURA SESSÃO PÚBLICA-RETOMADA', 'PREGÃO SUSPENSO', 'EDITAL PUBLICADO', 'PRAZO PARA MEMORIAIS', 'AGUARDANDO RECEBIMENTO DE PROPOSTAS', 'AGUARDANDO ABERTURA SESSÃO PÚBLICA-RETOMADA', 'ADJUDICAÇÃO AUTORIDADE', 'ELABORAÇÃO DA ATA', 'PRAZO PARA CONTRARRAZÕES', 'HOMOLOGAÇÃO'}
+print('STARTED!')
 
-situacao = 'EDITAL PUBLICADO'
-
+ocs_save = []
 for item in extraction:
     url = "https://www.bec.sp.gov.br/BEC_API/API/pregaoM/NegociacaoItemOC/{}".format(item['Codigo'])
     response = requests.request("GET", url, headers=headers)
     ocs = json.loads(response.text)
 
     for oc in ocs:
-        situacoes.add(oc['SITUACAO'])
-        if situacao == oc['SITUACAO']:
-            print(oc)
+        ocs_save.append(oc)
+        # situacoes.add(oc['SITUACAO'])
+        # if situacao == oc['SITUACAO']:
+        #     print(oc)
+        print(oc)
 
-print(situacoes)
+save_json_file(ocs_save, 'ocs', 'json')
+print('DONE!')
